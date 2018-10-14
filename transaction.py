d9a3ba0ee3653d9b3c4cf34783b980dc0eee36ca
Brandon Christof <brandonchristof@gmail.com>
2018-10-14 16:43:58 -0400

    Shell Transaction File

---
+++ transaction.py
@@ -0,0 +1,28 @@
+'''
+    Transaction
+'''
+
+class Transaction(object):
+    
+    def __init__(self):
+        pass
+
+    def execute():
+        pass
+
+    def prompt(s):
+        print(s)
+        pass    
+
+    def promptAndInput(s):
+        return s
+    
+
+    def checkValidNumber(name):
+        pass
+
+    def checkValidName(number):
+        pass        
+
+
+
