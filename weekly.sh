d26e67460ff146e7f383a1b998a9c7c327d8985e
Michael <molson.omichael@gmail.com>
2018-12-04 13:40:58 -0500

    added outline for weekly script

---
+++ weekly.sh
@@ -0,0 +1,17 @@
+## Weekly test script
+## CISC 327 Assignment 6
+## Michael Olson        20008033
+## Brandon Christof     200*****
+
+## Create the initial valid services file
+
+## Create the initial central services file
+
+## For 5 times
+
+    ## Save the old VSF and CSF files
+    ## Run the daily script
+
+## Save the final VSF and CSF files
+
+
